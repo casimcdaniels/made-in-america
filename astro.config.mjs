7dca1d9786a720b9460da9c8ea91a76124965a83
Casi McDaniels <casimcdaniels@gmail.com>
2025-12-01 01:48:07 -0500

    set base url in paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ export default defineConfig({
 	// Update this to match your GitHub username/organization
 	site: 'https://casimcdaniels.github.io',
 	// Update this to match your repository name, or use '/' for root deployment
-	base: '/made-in-america',
+	base: '/made-in-america/',
 	output: 'static',
 });
